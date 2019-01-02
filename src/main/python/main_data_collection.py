ec56194235581fcdc6cc2c42f3580a41b2ed9e0d
Humayun Kayesh <hkayesh@gmail.com>
2019-01-02 11:25:13 +1000

    Added Collector class

--- src/main/python/main_data_collection.py
+++ src/main/python/main_data_collection.py
@@ -1,4 +1,8 @@
-
+from data_collection.collector import Collector
 
 if __name__ == '__main__':
-    print('Hello')
+    collector = Collector()
+
+    html = collector.simple_get('https://www.babycenter.com.au/g25001383/expecting-a-baby-')
+
+    print(html)
